733c4f4a83478a737ae1f5fe3bc7951a7d23e353
Вадим <guts.vadim@gmail.com>
2015-11-25 15:08:42 +0200

    lesson 2

--- src/main/java/org/guts/MainController.java
+++ src/main/java/org/guts/MainController.java
@@ -1,11 +1,13 @@
 package org.guts;
 
+import org.guts.entity.Note;
 import org.guts.service.NotesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -28,10 +30,10 @@ public class MainController {
         return "notes";
     }
 
-    @RequestMapping("/notes/{noteId}")
-    public String note(@PathVariable(value = "noteId") long noteId, Model model) {
-        model.addAttribute("note", notesService.findNoteById(noteId));
-        return "note";
+    @RequestMapping(value = "/notes/add", method = RequestMethod.POST)
+    public String addNote(@RequestParam("title") String title, @RequestParam("text") String text) {
+        notesService.saveNote(new Note(title, text, 1));
+        return "redirect:/notes";
     }
 
 }
